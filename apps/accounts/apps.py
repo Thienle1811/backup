891ee372be2177287eaa57913ea6fa3b0327bc6c
Alex <alex@lolconsultants.com>
2025-05-23 09:44:31 +0700

    fix: update app config and deployment settings

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.accounts'
-    label = 'accounts'
+    name = "apps.accounts"            # Python import path
+    label = "accounts"                # what Django will call it
+    default_auto_field = "django.db.models.BigAutoField"
