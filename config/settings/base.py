79b87c6605ff9d109eb4cbc05c2a763ff1abf309
Alex <alex@lolconsultants.com>
2025-05-25 19:39:56 +0700

    Update settings and add reportlab dependency

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-s82%s1+6o&jcse_3)my^vspflc1$=+#*iqo*d@+=7boo*us3v9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-server-production-2d8a.up.railway.app', 'xn--ykhoaungbucnth-jtd8uv287bx0a.com.vn']
 
 # Application definition
 DJANGO_APPS = [
--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', SECRET_KEY)
